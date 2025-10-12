ca3127c0f512b3b8ceb6c8506c8d14833b9858c1
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2025-10-12 16:41:28 +0100

    Add initial Blazor projects and CRUD app
    This commit adds three Blazor projects: BlazorApp2, BlazorApp3, and BlazorCRUD. Each project includes solution and project files, component and layout files, configuration, and supporting assets. BlazorCRUD additionally implements CRUD functionality for Student entities, including EF Core context, models, migrations, and related Razor pages for create, read, update, and delete operations.

---
+++ BlazorCRUD/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCRUD.Models {
+
+	public class Student {
+		public int Id { get; set; }
+
+		[Required]
+		public string? Name { get; set; }
+
+		[Range(18, 49)]
+		public int Age { get; set; }
+
+		public DateTime? Birthday { get; set; }
+	}
+
+}
